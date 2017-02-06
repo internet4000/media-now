c2a59cf292b89630ec9ab15a2d50dbc24baafe34
Oskar Rough <oskar@rough.dk>
2017-02-06 15:28:58 +0100

    Improve Spotify search results by splitting `artist - title`

--- src/serializer-spotify.js
+++ src/serializer-spotify.js
@@ -1,10 +1,30 @@
 const fetch = require('isomorphic-fetch')
 
+// Take a query like "Artist - Track title"
+// and return {artist, title}
+const splitQuery = query => {
+	let q = decodeURI(query)
+	if (!q.includes(' - ')) {
+		return false
+	}
+	let [artist, ...track] = q.split(' - ')
+	track = track.join(' - ')
+	return {artist, track}
+}
+
 const buildURL = function (query) {
+	const endpoint = `https://api.spotify.com/v1/search`
 	const type = 'track'
 	const limit = 10
-	const endpoint = `https://api.spotify.com/v1/search`
-	return `${endpoint}?q=track:${query}&limit=${limit}&type=${type}&`
+
+	// Spotify returns much more precise results when
+	// artist and track are specified seperately.
+	let info = splitQuery(query)
+	if (info.artist && info.track) {
+		query = `artist:${info.artist}+track:${info.track}`
+	}
+
+	return `${endpoint}?limit=${limit}&type=${type}&q=${query}`
 }
 
 const fetchData = async function (id, args) {
@@ -49,7 +69,10 @@ const serialize = function (json) {
 	}
 
 	// Otherwise it was a single track.
-	return serializeOne(json)
+	if (json.id) {
+		return serializeOne(json)
+	}
+	return {error: 'no results'}
 }
 
 module.exports.fetchData = fetchData
